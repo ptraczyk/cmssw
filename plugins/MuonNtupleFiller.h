8c6d1b2749620e8314c4d1f9851be1cc98be9484
Piotr <piotraczyk@gmail.com>
2016-08-26 15:45:00 +0200

    a simple ntuple filler for muon timing info

---
+++ plugins/MuonNtupleFiller.h
@@ -0,0 +1,156 @@
+#ifndef RecoMuon_MuonIdentification_MuonNtupleFiller_H
+#define RecoMuon_MuonIdentification_MuonNtupleFiller_H
+
+/** \class MuonNtupleFiller
+ *  Dump muon information into a simple ntuple
+ *
+ *  $Date: 2011/04/06 09:56:30 $
+ *  $Revision: 1.4 $
+ *  \author P. Traczyk   
+ */
+
+// Base Class Headers
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+//#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+
+#include "SimDataFormats/Track/interface/SimTrack.h"
+#include "SimDataFormats/Track/interface/SimTrackContainer.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/TrackReco/interface/TrackExtraFwd.h"
+#include "DataFormats/TrackReco/interface/TrackToTrackMap.h"
+#include "DataFormats/MuonReco/interface/MuonTimeExtra.h"
+#include "DataFormats/MuonReco/interface/MuonTimeExtraMap.h"
+#include "RecoMuon/TrackingTools/interface/MuonSegmentMatcher.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "DataFormats/DTRecHit/interface/DTRecSegment4DCollection.h"
+#include "DataFormats/DTRecHit/interface/DTRecHitCollection.h"
+#include "DataFormats/CSCRecHit/interface/CSCSegmentCollection.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticle.h"
+
+#include <TROOT.h>
+#include <TSystem.h>
+#include "Riostream.h"    
+#include "TObject.h"
+#include <TTree.h>
+
+namespace edm {
+  class ParameterSet;
+  //  class Event;
+  class EventSetup;
+  class InputTag;
+}
+
+class TFile;
+class TTree;
+
+using namespace std;
+using namespace edm;
+using namespace reco;
+
+class MuonNtupleFiller : public edm::EDAnalyzer {
+//class MuonNtupleFiller : public edm::stream::EDProducer<> {
+public: 
+
+  explicit MuonNtupleFiller(const edm::ParameterSet&);
+  ~MuonNtupleFiller();
+  
+private:
+  virtual void beginJob() ;
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+
+  double iMass(reco::TrackRef imuon, reco::TrackRef iimuon);
+
+  // ----------member data ---------------------------
+
+  edm::ConsumesCollector *iC;
+  edm::InputTag TKtrackTags_; 
+  edm::InputTag MuonTags_; 
+  edm::InputTag TimeTags_; 
+  edm::InputTag SIMtrackTags_; 
+
+  string out, open;
+  bool theDebug;
+  bool doSim;
+  double theAngleCut;
+  double thePtCut;
+
+  edm::EDGetTokenT<reco::BeamSpot> beamSpotToken_;
+  edm::EDGetTokenT<reco::TrackCollection> trackToken_;
+  edm::EDGetTokenT<reco::MuonCollection> muonToken_;
+  edm::EDGetTokenT<reco::VertexCollection> vertexToken_;
+  edm::EDGetTokenT<reco::MuonTimeExtraMap> timeMapCmbToken_;
+  edm::EDGetTokenT<reco::MuonTimeExtraMap> timeMapDTToken_;
+  edm::EDGetTokenT<reco::MuonTimeExtraMap> timeMapCSCToken_;
+  edm::EDGetTokenT<GenParticleCollection> genParticleToken_;
+  edm::EDGetTokenT<TrackingParticleCollection> trackingParticleToken_;
+
+  Handle<reco::MuonCollection> MuCollection;
+  Handle<reco::MuonCollection> MuCollectionT;
+  Handle<reco::TrackCollection> TKTrackCollection;
+  Handle<reco::TrackCollection> STATrackCollection;
+  Handle<reco::TrackCollection> GLBTrackCollection;
+  Handle<reco::TrackCollection> PMRTrackCollection;
+  Handle<reco::TrackCollection> GMRTrackCollection;
+  Handle<reco::TrackCollection> FMSTrackCollection;
+  Handle<reco::TrackCollection> SLOTrackCollection;
+  Handle<edm::SimTrackContainer> SIMTrackCollection;
+
+  Handle<reco::MuonTimeExtraMap> timeMap1;
+  Handle<reco::MuonTimeExtraMap> timeMap2;
+  Handle<reco::MuonTimeExtraMap> timeMap3;
+  
+  // ROOT Pointers
+  TFile* hFile;
+  TTree* t;
+
+  // ***** Tree structure *******
+// generator info (if available)
+  bool hasSim;
+  int genCharge;
+  double genPt, genPhi, genEta;
+  int genBX;
+
+// event info
+  unsigned int event_run;
+  unsigned int event_lumi;
+  unsigned int event_event;
+  bool isCosmic;
+  bool isCollision;
+
+// muon ID
+  bool isPF;
+  bool isSTA;
+  bool isGLB;
+  bool isLoose;
+  bool isTight;
+
+// muon kinematics and track fit parameters
+  int charge;
+  double pt, phi, eta;
+  double dPt;
+  double dz;
+  double dxy;
+  
+// muon timing
+  int muNdof;
+  double muTime;
+  double muTimeErr;
+  int dtNdof;
+  double dtTime;
+  int cscNdof;
+  double cscTime;
+  int rpcNdof;
+  double rpcTime;
+  double rpcTimeErr;
+
+};
+#endif
---
+++ python/MuonNtupleFiller_cfi.py
@@ -0,0 +1,18 @@
+import FWCore.ParameterSet.Config as cms
+
+muonNtupleFiller = cms.EDAnalyzer("MuonNtupleFiller",
+
+    mctruthMatching = cms.bool(True),
+
+    Muons = cms.untracked.InputTag("muons"),
+    TKtracks = cms.untracked.InputTag("generalTracks"),
+    Timing = cms.untracked.InputTag("muons"),
+
+    # cosmic ID back-to-back angle cut 
+    angleCut = cms.double(0.02),
+    PtCut = cms.double(5.0),
+
+    open = cms.string('recreate'),
+    out = cms.string('muonNtuple.root'),
+    debug= cms.bool(False)
+)
